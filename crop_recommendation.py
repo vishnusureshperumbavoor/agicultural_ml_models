885aa57d1ae35c355bf426635e19bbbf652e8ad9
vishnusureshperumbavoor <vishnusureshperumbavoor@gmail.com>
2023-05-31 13:36:06 +0530

    removed background image for better ui

--- crop_recommendation.py
+++ crop_recommendation.py
@@ -7,21 +7,21 @@ st.set_page_config(
     page_icon="🌾"
 )
 
-def add_bg_from_local(image_file):
-    with open(image_file, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-    st.markdown(
-    f"""
-    <style>
-    .stApp {{
-        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
-        background-size: cover
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-    )
-add_bg_from_local('images/background_image.png')  
+# def add_bg_from_local(image_file):
+#     with open(image_file, "rb") as image_file:
+#         encoded_string = base64.b64encode(image_file.read())
+#     st.markdown(
+#     f"""
+#     <style>
+#     .stApp {{
+#         background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+#         background-size: cover
+#     }}
+#     </style>
+#     """,
+#     unsafe_allow_html=True
+#     )
+# add_bg_from_local('images/background_image.png')  
 
 
 
@@ -62,7 +62,7 @@ def main():
     try:
         if st.button('Predict'):
             pred = dtc_model.predict(data_new)
-            #st.balloons()
+            st.balloons()
             st.success("You can cultivate {}".format(pred[0]))
     except:
         st.warning("You can't cultivate crops in this land")
